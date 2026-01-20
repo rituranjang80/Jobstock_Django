2e8c89a10a402f49eea57f3fdfe38325d05d183a
rituranjang <110986171+rituranjang@users.noreply.github.com>
2026-01-20 13:08:39 +0530

    hideUI

--- Jobstock/urls.py
+++ Jobstock/urls.py
@@ -26,11 +26,21 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Jobstock API",
+#         default_version='v1',
+#         description="API documentation for Jobstock platform",
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
+
 schema_view = get_schema_view(
     openapi.Info(
-        title="Jobstock API",
+        title=".",
         default_version='v1',
-        description="API documentation for Jobstock platform",
+        description="API",
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
